e949aab3a0d5929c2f4520d606f081df4f272409
ahnyunki <ahnyunki@naver.com>
2023-10-05 09:33:34 +0900

    Presentation Layer 테스트(1)

--- src/main/java/sampl/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sampl/cafekiosk/spring/api/controller/product/ProductController.java
@@ -2,7 +2,9 @@ package sampl.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sampl.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sampl.cafekiosk.spring.api.service.product.ProductService;
 import sampl.cafekiosk.spring.api.service.product.response.ProductResponse;
 
@@ -13,6 +15,11 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping("/api/v1/product/new")
+    public void createProduct(ProductCreateRequest request) {
+        productService.createProduct(request);
+    }
 
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
--- src/main/java/sampl/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sampl/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sampl.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sampl.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sampl.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sampl.cafekiosk.spring.domain.product.Product;
 import sampl.cafekiosk.spring.domain.product.ProductRepository;
@@ -10,11 +12,43 @@ import sampl.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Query
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    // 동시성 이슈
+    // UUID
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProduct();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -24,5 +58,4 @@ public class ProductService {
                 .collect(Collectors.toList());
 
     }
-
 }
